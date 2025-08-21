9b36b9607fe54dbed6a04965e433a820d4034f75
Mohamad Al-Sadah <Mideaxinq@gmail.com>
2025-08-21 18:45:51 +0300

    added routing

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const adminLayout = () => {
+    return;
+    <div className="admin-layout">
+        MobileSidebar
+        <aside className="w-full max-w-[270px] hidden lg:block">Sidebar </aside>
+    </div>;
+};
+
+export default adminLayout;
